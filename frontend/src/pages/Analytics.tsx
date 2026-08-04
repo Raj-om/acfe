23df8e0d906cb996a3765afd6570856b0da3ee98
Raj sekhar parhi <rapr1461@gmail.com>
2026-08-04 13:16:12 +0530

    feat: add frontend/src/pages/Analytics.tsx

---
+++ frontend/src/pages/Analytics.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlassCard from '../components/common/GlassCard';
+
+const Analytics = () => {
+  return (
+    <div className="dashboard-grid">
+      <GlassCard className="col-span-12" title="Historical Analytics">
+        <p>Long-term data visualization and reports will go here.</p>
+      </GlassCard>
+    </div>
+  );
+};
+
+export default Analytics;
