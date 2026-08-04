16d3290efec564fbdf864dfd732729ee6776d42c
Raj sekhar parhi <rapr1461@gmail.com>
2026-08-04 13:16:16 +0530

    feat: add frontend/src/pages/Sensors.tsx

---
+++ frontend/src/pages/Sensors.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlassCard from '../components/common/GlassCard';
+
+const Sensors = () => {
+  return (
+    <div className="dashboard-grid">
+      <GlassCard className="col-span-12" title="Sensor Management">
+        <p>Detailed sensor configuration will go here.</p>
+      </GlassCard>
+    </div>
+  );
+};
+
+export default Sensors;
